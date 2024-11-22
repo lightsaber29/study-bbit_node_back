c0d79dc416e8bb926e72b00f233c438e945942fc
Hyobeom Jang <gyqjajang@gmail.com>
2024-11-22 16:40:51 +0900

    fix : api prefix 변경

--- src/routes/livekitRoutes.js
+++ src/routes/livekitRoutes.js
@@ -11,12 +11,12 @@ import {
 const router = express.Router();
 
 router.post('/livekit/webhook', handleWebhook);
-router.get('/api/connection-details', getConnectionDetails);
+router.get('/api/express/connection-details', getConnectionDetails);
 
-router.post('/api/create-room', createRoom);
-router.get('/api/list-rooms', listRooms);
-router.delete('/api/delete-room/:name', deleteRoom);
+router.post('/api/express/create-room', createRoom);
+router.get('/api/express/list-rooms', listRooms);
+router.delete('/api/express/delete-room/:name', deleteRoom);
 
-router.get('/api/list-participants/:roomName', listParticipants);
+router.get('/api/express/list-participants/:roomName', listParticipants);
 
 export default router;
